5ae9b870b1f06a7e761a489c7fd03063b06595c5
vaheshmavonyan34 <vaheshmavonyan34@gmail.com>
2026-04-01 23:42:09 +0400

    fix bug 2and6:01.04.2026

--- app/bot/main.py
+++ app/bot/main.py
@@ -119,7 +119,7 @@ async def start_command(message: types.Message) -> None:
     welcome_message = f"""
 🎮 Добро пожаловать в PlayStation Store, {user.first_name}!
 
-Здесь вы можете:
+Здесь вы можете23:
 • Просматривать каталог игр PlayStation
 • Фильтровать товары по категориям и ценам
 • Добавлять игры в избранное
